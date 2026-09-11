4eb9fe19f8912046c9f3384c8767d33360e51ce7
Prasanta Kumar Dash <prasantadash3@gmail.com>
2026-09-11 15:25:52 +0530

    Update documentation

--- sidebars.js
+++ sidebars.js
@@ -77,12 +77,6 @@ const sidebars = {
       collapsed: true,
       items: ['fixed-deposit-sdk/play-store-permission'],
     },
-    {
-    type: 'category',
-      label: '11. Prasanta Dash,
-      collapsed: true,
-      items: ['fixed-deposit-sdk/Prasanta'],
-    },
   ],
 };
 
